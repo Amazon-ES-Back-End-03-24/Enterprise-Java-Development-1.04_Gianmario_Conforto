a6cd5209ee8d17a9bdb20bf173b2eb12c171897a
Gianmario <gconfortom@gmail.com>
2024-03-23 18:17:00 +0100

    Ejercicio 1 y 2

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,52 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
 
-        int[] numbers = {-15, 5, 12, 20, 35};
+        exerciseOne();
+        exerciseTwo();
+
+    }
+    private static void exerciseOne() {
+        int[] numbers = {-1, 5, 12, -15, 20, 35, -5};
         int maxnum = 0, minnum = 0;
+        int difference = 0;
 
         System.out.println("Exercise 1");
 
-        for (int i=0; i < numbers.length; i++) {
+        for (int i = 0; i < numbers.length; i++) {
             if (numbers[i] > maxnum)
                 maxnum = numbers[i];
 
             if (numbers[i] < minnum)
                 minnum = numbers[i];
+
+            difference = maxnum - minnum;
         }
-        int difference = maxnum - minnum;
 
         System.out.println("The difference is: " + difference);
+    }
+
+    private static void exerciseTwo() {
+        int[] numbers = {-1, 5, 12, -15, 20, 35, -5};
+        int min1 = numbers[0];
+        int min2 = numbers[0];
 
         System.out.println("Exercise 2");
 
-        int minnum2 = 0;
-       git a
         for (int i=0; i < numbers.length; i++) {
-            if (numbers[i] < minnum2)
-                minnum2 = numbers[i];
+            if (numbers[i] < min1) {
+                min2 = min1;
+                min1 = numbers[i];
+            }
 
-            if (numbers[i] > minnum)
-                minnum = numbers[i];
-
-            System.out.println("The smallest numbers are: " + minnum + " and " + minnum2);
+            else if (numbers[i] < min2) {
+                min2 = numbers[i];
+            }
         }
+        System.out.println("The smallest numbers are: " + min1 + " and " +min2);
     }
+
+
 
 }
