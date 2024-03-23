25c728fc2de8b0beff3eba5bff119c4776596084
Gianmario <gconfortom@gmail.com>
2024-03-23 15:29:00 +0100

    prueba 2

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,8 @@ public class Main {
         int[] numbers = {-15, 5, 12, 20, 35};
         int maxnum = 0, minnum = 0;
 
+        System.out.println("Exercise 1");
+
         for (int i=0; i < numbers.length; i++) {
             if (numbers[i] > maxnum)
                 maxnum = numbers[i];
@@ -14,5 +16,20 @@ public class Main {
         int difference = maxnum - minnum;
 
         System.out.println("The difference is: " + difference);
+
+        System.out.println("Exercise 2");
+
+        int minnum2 = 0;
+       git a
+        for (int i=0; i < numbers.length; i++) {
+            if (numbers[i] < minnum2)
+                minnum2 = numbers[i];
+
+            if (numbers[i] > minnum)
+                minnum = numbers[i];
+
+            System.out.println("The smallest numbers are: " + minnum + " and " + minnum2);
+        }
     }
+
 }
